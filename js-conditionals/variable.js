f27c9f265d4220c25cddc055704ee00bbce4190b
Mohyminul Islam <mohyminulmunna23@gmail.com>
2025-08-01 12:30:15 +0600

    I just learn Variable on js

---
+++ js-conditionals/variable.js
@@ -0,0 +1,15 @@
+let applePrice = 20;
+let orangePrice = 30;
+let totalPrice = applePrice + orangePrice;
+console.log("your result is " + totalPrice)
+
+// ------------------------------------------------
+let first = 0.1;
+let second = 2.1;
+let result = first + second;
+console.log(result.toFixed(1))
+// ------------------------------------------------
+
+let price = 20;
+price /= 30;
+console.log(price.toFixed(2))
